df8bc3a1f1238e3974de2522a80aa94ea82e0e17
jalen <jalenchuh@gmail.com>
2023-12-25 12:45:23 +0800

    :art: add clang-format

--- include/ai_player.hpp
+++ include/ai_player.hpp
@@ -2,12 +2,12 @@
 
 #include "player.hpp"
 
-class AiPlayer : public Player {
+class AiPlayer: public Player {
 public:
-  AiPlayer() = default;
-  explicit AiPlayer(std::string name);
-  ~AiPlayer() = default;
+    AiPlayer() = default;
+    explicit AiPlayer(std::string name);
+    ~AiPlayer() = default;
 
 private:
-  int LevelUp(int value) override;
+    int LevelUp(int value) override;
 };
--- include/player.hpp
+++ include/player.hpp
@@ -4,26 +4,26 @@
 
 class Player {
 public:
-  Player() = default;
-  explicit Player(std::string name);
-  ~Player() = default;
+    Player() = default;
+    explicit Player(std::string name);
+    ~Player() = default;
 
-  /**
-   * @brief 玩家进行游戏
-   * @return level
-   */
-  int Play();
+    /**
+     * @brief 玩家进行游戏
+     * @return level
+     */
+    int Play();
 
-  // 返回玩家等级
-  int GetLevel();
+    // 返回玩家等级
+    int GetLevel();
 
-  std::string name_; // 玩家名字
-  int level_ = 0;    // 玩家等级
+    std::string name_; // 玩家名字
+    int level_ = 0;    // 玩家等级
 
 private:
-  /**
+    /**
    * @brief 玩家升级
    * @return 玩家当前等级
    */
-  virtual int LevelUp(int value);
+    virtual int LevelUp(int value);
 };
